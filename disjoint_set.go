24b5862795a8d1529a132349a8b123be1d01a60a
Robert Johnson <robert.johnson@postmates.com>
2017-03-21 14:52:52 -0700

    Make Find/Union more object-oriented

--- disjoint_set.go
+++ disjoint_set.go
@@ -1,35 +1,36 @@
 package disjointset
 
-import "errors"
-
+// DisjointSet that implements the disjoint set ADT with API Find/Union.
 type DisjointSet struct {
 	Value  interface{}
 	parent *DisjointSet
 	rank   int
 }
 
+// MakeSet returns a pointer to a new DisjoinSet that stores the given
+// value. The returned DisjointSet has no parent.
 func MakeSet(value interface{}) *DisjointSet {
 	return &DisjointSet{value, nil, 0}
 }
 
-func Find(ds *DisjointSet) (*DisjointSet, error) {
-	if ds == nil {
-		return nil, errors.New("Invalid pointer for Find")
-	}
-
+// Find returns a pointer to the DisjointSet that is the parent of this
+// DisjointSet. If this DisjointSet has no parent, then Find will return
+// this DisjointSet.
+func (ds *DisjointSet) Find() (*DisjointSet, error) {
 	if ds.parent == nil {
 		return ds, nil
 	}
 
-	return Find(ds.parent)
+	return ds.parent.Find()
 }
 
+// Union merges the two DisjointSets into one DisjointSet.
 func Union(x *DisjointSet, y *DisjointSet) error {
-	xRoot, xerr := Find(x)
+	xRoot, xerr := x.Find()
 	if xerr != nil {
 		return xerr
 	}
-	yRoot, yerr := Find(y)
+	yRoot, yerr := y.Find()
 	if yerr != nil {
 		return yerr
 	}
